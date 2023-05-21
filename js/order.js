7a1d3e1dae29d7c113b04d87c91aec21a3818822
aloysiussenjaya <aloysiussenjaya@gmail.com>
2023-05-21 15:38:56 +0700

    Configure js for add new type pop up form

--- js/order.js
+++ js/order.js
@@ -1,14 +1,14 @@
 // Pop Up
 $("#addNewVendorModal").on("show.bs.modal", function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
-  var modal = $(this);
-  modal.find(".modal-title").text("Add New Vendor");
 });
 
 $("#addNewCompanyModal").on("show.bs.modal", function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
-  var modal = $(this);
-  modal.find(".modal-title").text("Add New Vendor");
+});
+
+$("#addNewTypeModal").on("show.bs.modal", function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
 });
 
 // Data Tables
