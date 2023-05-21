de3f3697f88cd13097f1f21b42cf5ddb22398b00
aloysiussenjaya <aloysiussenjaya@gmail.com>
2023-05-21 15:37:32 +0700

    Configure js for add new company pop up form

--- js/order.js
+++ js/order.js
@@ -1,9 +1,17 @@
+// Pop Up
 $("#addNewVendorModal").on("show.bs.modal", function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
   var modal = $(this);
   modal.find(".modal-title").text("Add New Vendor");
 });
 
+$("#addNewCompanyModal").on("show.bs.modal", function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var modal = $(this);
+  modal.find(".modal-title").text("Add New Vendor");
+});
+
+// Data Tables
 $(document).ready(function () {
-    $('#table-list-of-orders').DataTable();
+  $("#table-list-of-orders").DataTable();
 });
